5032685d28cc983e011b3b8009b77b5abbc4aa4e
yosraelmasry-dev <74475192+Yosra010@users.noreply.github.com>
2025-09-22 14:04:45 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById('contact-form');
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('شكراً لتواصلك! سيتم الرد عليك قريباً.');
-  form.reset();
-});
